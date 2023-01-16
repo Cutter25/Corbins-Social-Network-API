07f215a34cbeb3669146ec34a3f9b1710422e25e
Corbin Cutter <Corbin@Lisas-iMac.lan>
2023-01-16 00:40:40 -0500

    created basic boilerplate routes to setup controllers

---
+++ routes/api/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+const userRoutes = require('./userRoutes');
+const thoughtRoutes = require('./thoughtroutes');
+
+router.use('/user', userRoutes);
+router.use('/thought', thoughtRoutes);
+
+module.exports = router;
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+const { getAllUsers, getUsersById, createUser, updateUsers, deleteUsers,
+    addFriend, deleteFriend
+} = require('../../controllers/userController');
+
+router.route('/').get(getAllUsers).post(createUser);
+
+router.route('/:id').get(getUsersById).put(updateUsers).delete(deleteUsers);
+
+router.route('/:id/friends/:friendId').post(addFriend).delete(deleteFriend)
+
+module.exports = router; 
